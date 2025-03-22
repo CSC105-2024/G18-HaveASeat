2fc6ead08cb0e9873cb1dbe32890ae9da4c93090
ttwrpz <smrmakeyt@gmail.com>
2025-03-23 01:28:51 +0700

    feat(frontend): Added delete user overlay

--- frontend/src/overlay/user/delete.jsx
+++ frontend/src/overlay/user/delete.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { createModalHook } from "@/hooks/use-modal.jsx";
+import { Button } from "@/components/ui/button.jsx";
+import { useModalStore } from "@/store/modal.jsx";
 
 function UserDeleteOverlay(props) {
+  const { closeModal } = useModalStore();
+
+  function onDelete() {
+    try {
+      //TODO: Implement the logic
+      alert("Are you sure you want to delete this user?");
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
-    <div></div>
+    <div className="flex flex-col md:flex-row gap-4">
+      <Button onClick={onDelete} className="w-full flex-1" variant="destructive">Delete</Button>
+      <Button onClick={() => closeModal('user-delete')} className="w-full flex-1" variant="secondary">Back</Button>
+    </div>
   );
 }
 
 const useUserDeleteOverlay = createModalHook(
   UserDeleteOverlay,
-  'user-delete',
-  'User Delete',
-  '',
+  "user-delete",
+  "Delete Confirmation",
+  <Fragment>
+    Are you sure to delete this user?{" "}
+    <span className="text-red-500">This action cannot be undone.</span>
+  </Fragment>,
 );
 
 export { useUserDeleteOverlay, UserDeleteOverlay };
