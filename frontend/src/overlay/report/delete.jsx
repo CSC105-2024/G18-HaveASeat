8c103af0a6937b0752034a1a9f47e64d3e4d6c92
Tienkarnsinee <145735567+Tienkarnsinee@users.noreply.github.com>
2025-03-22 18:42:10 +0000

    feat(frontend): Added delete & ignore report overlay

--- frontend/src/overlay/report/delete.jsx
+++ frontend/src/overlay/report/delete.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { createModalHook } from "@/hooks/use-modal.jsx";
+import { useModalStore } from "@/store/modal.jsx";
+import { Button } from "@/components/ui/button.jsx";
 
 function ReportDeleteOverlay(props) {
+  const { closeModal } = useModalStore();
+
+  function onDelete() {
+    try {
+      //TODO: Implement the logic
+      alert("Are you sure you want to delete this report?");
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
-    <div></div>
+    <div className="flex flex-col md:flex-row gap-4">
+      <Button onClick={onDelete} className="w-full flex-1" variant="destructive">Delete</Button>
+      <Button onClick={() => closeModal('report-delete')} className="w-full flex-1" variant="secondary">Back</Button>
+    </div>
   );
 }
 
 const useReportDeleteOverlay = createModalHook(
   ReportDeleteOverlay,
   'report-delete',
-  'Report Delete Confirmation',
-  '',
+  "Delete Confirmation",
+  <Fragment>
+    Are you sure to delete this report?{" "}
+    <span className="text-red-500">This action cannot be undone.</span>
+  </Fragment>,
 );
 
 export { useReportDeleteOverlay, ReportDeleteOverlay };
