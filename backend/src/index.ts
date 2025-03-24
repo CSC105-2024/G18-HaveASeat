3df6581e0c7b075c5a5e08c6496205d6910beb30
ttwrpz <smrmakeyt@gmail.com>
2025-03-24 22:28:38 +0700

    chore(ci): Changed Backend to test with the tsc build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,16 @@ import { Hono } from "hono";
 import { trimTrailingSlash } from "hono/trailing-slash";
 import { compress } from "hono/compress";
 import { logger } from "hono/logger";
+import { serveStatic } from "@hono/node-server/serve-static";
 
 const app = new Hono({ strict: true });
 
 app.use(compress());
 app.use(trimTrailingSlash());
 app.use(logger());
+
+app.use('*', serveStatic({ root: '../public' }))
+
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
