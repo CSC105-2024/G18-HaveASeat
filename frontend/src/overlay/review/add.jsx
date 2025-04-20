15ee113dfc546f1f4598944d083c15354b598c42
ttwrpz <smrmakeyt@gmail.com>
2025-04-20 18:21:14 +0700

    feat(frontend): Added review stars hovering effects

--- frontend/src/overlay/review/add.jsx
+++ frontend/src/overlay/review/add.jsx
@@ -3,18 +3,26 @@ import { createModalHook } from "@/hooks/use-modal.jsx";
 import { IconStar } from "@tabler/icons-react";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea.jsx";
+import { useModalStore } from "@/store/modal.jsx";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip.jsx";
+import { cn } from "@/lib/utils.js";
 
 /**
  * @typedef {Object} ReviewAddOverlayProps
  * @property {boolean} [isReply]
  */
 
+const pluralRules = new Intl.PluralRules("en-US");
+
 /**
  * @param {ReviewAddOverlayProps} props
  * @returns {JSX.Element}
  */
 function ReviewAddOverlay({ isReply = false }) {
+  const { closeModal } = useModalStore();
+
   const [rating, setRating] = useState(0);
+  const [hoverRating, setHoverRating] = useState(0);
 
   if (isReply) {
     return (
@@ -27,8 +35,17 @@ function ReviewAddOverlay({ isReply = false }) {
         </div>
 
         <div className="flex justify-between gap-4">
-          <Button type="submit" className="flex-1">Reply</Button>
-          <Button type="button" variant="secondary" className="flex-1">Close</Button>
+          <Button type="submit" className="flex-1">
+            Reply
+          </Button>
+          <Button
+            type="button"
+            variant="secondary"
+            className="flex-1"
+            onClick={() => closeModal("review-add")}
+          >
+            Close
+          </Button>
         </div>
       </div>
     );
@@ -37,21 +54,37 @@ function ReviewAddOverlay({ isReply = false }) {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex justify-center space-x-1">
-        {[1, 2, 3, 4, 5].map((num) => (
-          <IconStar
-            key={num}
-            onClick={() => setRating(num)}
-            className={`size-8 cursor-pointer transition-colors duration-150 ${
-              num <= rating ? "text-yellow-400 fill-yellow-400" : "text-gray-300"
-            }`}
-          />
-        ))}
+        <TooltipProvider>
+          {[1, 2, 3, 4, 5].map((number) => (
+            <Tooltip key={number}>
+              <TooltipTrigger asChild>
+                <IconStar
+                  onClick={() => setRating(number)}
+                  onMouseEnter={() => setHoverRating(number)}
+                  onMouseLeave={() => setHoverRating(0)}
+                  onTouchStart={() => setHoverRating(number)}
+                  onTouchEnd={() => setHoverRating(0)}
+                  className={cn(
+                    "size-8 cursor-pointer transition-colors duration-150",
+                    number <= (hoverRating || rating)
+                      ? "fill-yellow-400 text-yellow-400"
+                      : "text-gray-300",
+                  )}
+                />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>
+                  {number}{" "}
+                  {pluralRules.select(number) === "one" ? "Star" : "Stars"}
+                </p>
+              </TooltipContent>
+            </Tooltip>
+          ))}
+        </TooltipProvider>
       </div>
 
       <div className="space-y-4">
-        <h2 className="text-xl font-semibold">
-          Description
-        </h2>
+        <h2 className="text-xl font-semibold">Description</h2>
         <Textarea
           className="bg-gray-50/50"
           placeholder="Describe what’s your experience of this bar like?"
@@ -59,8 +92,17 @@ function ReviewAddOverlay({ isReply = false }) {
       </div>
 
       <div className="flex justify-between gap-4">
-        <Button type="submit" className="flex-1">Submit</Button>
-        <Button type="button" variant="secondary" className="flex-1">Close</Button>
+        <Button type="submit" className="flex-1">
+          Submit
+        </Button>
+        <Button
+          type="button"
+          variant="secondary"
+          className="flex-1"
+          onClick={() => closeModal("review-add")}
+        >
+          Close
+        </Button>
       </div>
     </div>
   );
@@ -68,9 +110,12 @@ function ReviewAddOverlay({ isReply = false }) {
 
 const useReviewAddOverlay = createModalHook(
   ReviewAddOverlay,
-  'review-add',
-  ({isReply}) => isReply ? 'Reply Review' : 'Review',
-  ({isReply}) => isReply ? 'This is the best way to interact with your customers' : 'Tell us more about this place!',
+  "review-add",
+  ({ isReply }) => (isReply ? "Reply" : "Review"),
+  ({ isReply }) =>
+    isReply
+      ? "This is the best way to interact with your customers"
+      : "Tell us more about this place!",
 );
 
 export { useReviewAddOverlay, ReviewAddOverlay };
