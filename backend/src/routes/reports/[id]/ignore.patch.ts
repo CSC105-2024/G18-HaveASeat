be37599bc1919233ccdaf8b9fe7dd4240ac9ac0b
ttwrpz <smrmakeyt@gmail.com>
2025-05-07 00:19:34 +0700

    doc(backend): Added all available endpoint section
    chore(frontend): Changed from @vitejs/plugin-react to @vitejs/plugin-react-swc

---
+++ backend/src/routes/reports/[id]/ignore.patch.ts
@@ -0,0 +1,15 @@
+import type { Context } from "hono";
+import { authMiddleware } from "@/middlewares/auth.middleware.js";
+import type { AppEnv } from "@/types/env.js";
+
+export default async function (c: Context<AppEnv>) {
+  await authMiddleware(c, async () => {})
+
+  try {
+    const user = c.get('user');
+    return c.json({});
+  } catch (error) {
+    console.error('Page error:', error)
+    return c.json({ error: 'Internal server error' }, 500)
+  }
+}
